36334d241599c5f62e6c691648daa2f02bd792ec
Sanskar Choubey <choubeysanskar03@gmail.com>
2020-12-01 01:35:34 +0530

    added output textfield

--- apps.js
+++ apps.js
@@ -1,8 +1,18 @@
 var btnTranslate = document.querySelector(".btn");
 var inputField = document.querySelector(".input-field");
+var outputField = document.querySelector(".output-field");
+
+
+// outputField.innerText = "I am Sanskar" +" "+ "nwheuirhiusiu";
+
+function urlGenerator(text){
+        var generatedUrl = url + "?" + "text=" + inputField.value;
+        console.log(generatedUrl);
+}
+
 
 btnTranslate.addEventListener("click", function btnEventHandler(){
-    console.log("Clicked");
-    console.log(inputField.value);
+   outputField.innerText = "Translated:" + inputField.value;
+   urlGenerator();
 })
 

aed36aa04af066fd7b03752cbfa3d42c1cffca2d
Sanskar Choubey <choubeysanskar03@gmail.com>
2020-12-01 17:10:52 +0530

    Done

--- apps.js
+++ apps.js
@@ -1,27 +1,41 @@
+//References to the textarea,button and output div
+
 var btnTranslate = document.querySelector(".btn");
 var inputField = document.querySelector(".input-field");
 var outputField = document.querySelector(".output-field");
 
+//Declaration of some variables for further use
 
-// outputField.innerText = "I am Sanskar" +" "+ "nwheuirhiusiu";
-
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var url = "https://api.funtranslations.com/translate/minion.json";
 var text = inputField.value;
+
+//URL-Generator Function
 
 function urlGenerator(text) {
         return url + "?" + "text=" + inputField.value;
         console.log(generatedUrl);
 }
 
+//Error Handler Function
+
 function errorHandler(error) {
         console.log("Some error occured",error);
         alert("The server is down!Sorry for the inconvience");
 }
 
+//Function to fetch the URL
+
 function fetchURL() {
-        fetch(urlGenerator()).then(response => response.json()).then(JSON => console.log(JSON.contents.translated)).catch(errorHandler())
+        fetch(urlGenerator())
+        .then(response => response.json())
+        .then(JSON => {
+                var translatedText = JSON.contents.translated;
+                outputField.innerHTML = translatedText})
+        .catch(errorHandler)
 }
 
+//Event handler
 btnTranslate.addEventListener("click", function btnEventHandler() {
         console.log(fetchURL());     
 })

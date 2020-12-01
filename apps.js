0d5f02bfe86636375baac44ca98750ef79749e16
Sanskar Choubey <choubeysanskar03@gmail.com>
2020-12-01 14:06:41 +0530

    intergrate fetch() call

--- apps.js
+++ apps.js
@@ -5,14 +5,18 @@ var outputField = document.querySelector(".output-field");
 
 // outputField.innerText = "I am Sanskar" +" "+ "nwheuirhiusiu";
 
-function urlGenerator(text){
-        var generatedUrl = url + "?" + "text=" + inputField.value;
-        console.log(generatedUrl);
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var text = inputField.value;
+
+function urlGenerator(text) {
+       return url + "?" + "text="+ inputField.value;
+       console.log(generatedUrl);
 }
 
+function fetchURL(){
+        fetch(urlGenerator()).then(response => response.json()).then(JSON => console.log(JSON));
+}
 
-btnTranslate.addEventListener("click", function btnEventHandler(){
-   outputField.innerText = "Translated:" + inputField.value;
-   urlGenerator();
+btnTranslate.addEventListener("click", function btnEventHandler() {
+       console.log(fetchURL());
 })
-
